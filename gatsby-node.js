3d2fb8b3090d97fd32bf6da72ff040f8aad030f1
markjconlon <markjconlon@gmail.com>
2019-03-23 16:32:04 -0400

    ability to create pages from markdown posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+
+exports.createPages = ({boundActionCreators, graphql}) => {
+  const {createPage} = boundActionCreators;
+
+  const postTemplate = path.resolve('src/templates/post.js');
+
+  return graphql(`{
+    allMarkdownRemark{
+      edges {
+        node {
+          html
+          id
+          frontmatter{
+            path
+            title
+            date
+          }
+        }
+      }
+    }
+  }`)
+  .then(result => {
+    if(result.errors){
+      return Promise.reject(res.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach( ({node}) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate
+      })
+    })
+  })
+}
