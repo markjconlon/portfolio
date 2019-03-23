64bed9804edcd3f1c6d21070d2d9c0161085b02c
markjconlon <markjconlon@gmail.com>
2019-03-23 16:41:18 -0400

    changed postTemplate to a functional component

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,41 +1,27 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
+import Header from '../components/header';
 
-export default class PostTemplate extends React.Component {
-  render(){
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
-
-    return(
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
-      </Layout>
-    )
-  }
+export default function PostTemplate({data}){
+  const {markdownRemark: post} = data;
+  return (
+    <div>
+      <Header siteTitle={"markjconlon.com"}/>
+      <h1>{post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{__html: post.html }} />
+    </div>
+  )
 }
+
+export const postQuery = graphql`
+  query BlogPostByPath($path: String!){
+    markdownRemark(frontmatter: { path: { eq: $path} }){
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+`
