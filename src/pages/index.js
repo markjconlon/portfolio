2f2d1c278859cbd1b7f7ad698c1568112495d1f2
markjconlon <markjconlon@gmail.com>
2019-03-31 20:52:05 -0400

    created helpers.js added devices sizes for media queries and used them to convert index.js to styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,56 @@
 import React from "react"
+import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { device } from "../components/helpers"
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 90vh;
+
+  @media ${device.tablet}{
+    align-items: space-around;
+    justify-content: center;
+    flex-direction: column;
+  }
+`
+
+const ImgContainer = styled.div`
+  height: 300px;
+  width: 300px;
+  margin-right: 2.9rem;
+  @media ${device.tablet}{
+    margin-bottom: 1.45rem;
+    margin-right: 0;
+  }
+`
+
+const AboutCopy = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  margin-bottom: 1.45rem;
+  font-family: "Open Sans";
+
+  @media ${device.tablet}{
+    flex-direction: column-reverse;
+  }
+`
+
+const AboutCopyP = styled.p`
+  @media ${device.tablet}{
+    margin: 0;
+  }
+`
+
+const AboutCopyH3 = styled.h3`
+  @media ${device.tablet}{
+    margin-bottom: 1.45rem;
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -12,38 +61,20 @@ const IndexPage = () => (
         maxWidth: 960,
       }}
     >
-      <div
-        style={{
-          display: `flex`,
-          alignItems: `center`,
-          justifyContent: `space-around`,
-          height: `90vh`,
-        }}
-      >
-        <div
-          style={{
-            height: `300px`,
-            width: `300px`,
-            marginRight: `2.9rem`,
-          }}
-        >
+      <Main>
+        <ImgContainer>
           <div style={{ width: `300px`, marginBottom: `1.45rem` }}>
             <Image />
           </div>
-        </div>
+        </ImgContainer>
 
-        <div>
-          <p
-            style={{
-              display: `flex`,
-              flexDirection: `column`,
-              width: `60%`,
-            }} >
+        <AboutCopy>
+          <AboutCopyP>
             I am a software developer working out of Toronto. The majority of my time at work is spent on a React-Redux application using a C# backend. I am accomplished at wearing many hats from developer operations to maintaining highly performant, responsive web applications.
-          </p>
-          <h3 style={{margin: `0`,}}>Let's build something great together!</h3>
-        </div>
-      </div>
+          </AboutCopyP>
+          <AboutCopyH3>Let's build something great together!</AboutCopyH3>
+        </AboutCopy>
+      </Main>
     </div>
   </Layout>
 )
