ec5e713610e73a185a3b90a522df3c2dc459b64c
markjconlon <markjconlon@gmail.com>
2019-03-31 20:10:25 -0400

    rewrote index page wiht styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,44 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Welcome all,</h1>
-    <p>I am a fullstack developer working for ASTOUND. The majority of my time at work is spent on a React-Redux application using a C# backend. This project is a client portal that integrates with Salesforce and uses Elastic Search for our asset catalog with tens of thousands of assets.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+      }}
+    >
+      <div
+        style={{
+          display: `flex`,
+          alignItems: `center`,
+          justifyContent: `space-around`,
+          height: `90vh`,
+        }}
+      >
+        <div
+          style={{
+            height: `300px`,
+            width: `300px`,
+            marginRight: `2.9rem`,
+          }}
+        >
+          <div style={{ width: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div>
+        </div>
+
+        <div>
+          <p
+            style={{
+              display: `flex`,
+              flexDirection: `column`,
+              width: `60%`,
+            }} >
+            I am a software developer working out of Toronto. The majority of my time at work is spent on a React-Redux application using a C# backend. I am accomplished at wearing many hats from developer operations to maintaining highly performant, responsive web applications.
+          </p>
+          <h3 style={{margin: `0`,}}>Let's build something great together!</h3>
+        </div>
+      </div>
     </div>
   </Layout>
 )
